60272c361b183a8f4a2e6deb6bca9d3a1547c3ff
Pedro Ferraresi <ferraresi.pedro@gmail.com>
2021-05-02 13:43:12 -0300

    Created App Basic structure

---
+++ src/core/components/Button/index.tsx
@@ -0,0 +1,8 @@
+import react from 'react';
+import './styles.scss';
+
+const Button = () => (
+  <input type="text" value="Botão"/>
+);
+
+export default Button;
---
+++ src/core/components/Navbar/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.scss';
+
+const Navbar = () => (
+  <div>
+    <h1>Navbar</h1>
+  </div>
+);
+
+export default Navbar;
